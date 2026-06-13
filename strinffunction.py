cc6ffe0e8a400212de1cb92d98c62416d26a460a
Sambhavi Simbal <simbalsambhavi@gmail.com>
2026-06-14 00:04:42 +0530

    changes made

--- strinffunction.py
+++ strinffunction.py
@@ -4,3 +4,14 @@ print(length)
 print(fruit[0:3])
 print(fruit[0:len(fruit)-2])
 print(fruit[0:-1])
+print(fruit[1:3])
+for i in fruit:
+ print(i)
+#  strings are immutable
+a="harry" 
+# i cannot change this a not so if i need to perform some changes
+# i need to perform string operations 
+print(a.upper())
+print(a.lower())
+# string functins returns a new string instead of changing the original
+
